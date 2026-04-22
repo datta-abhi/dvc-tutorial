a91174dfd8a21bd0ff4ec568955ef5f52e312a6a
datta-abhi <rookbee91@gmail.com>
2026-04-22 23:34:00 +0530

    employee roles column added

--- data-ingestion.py
+++ data-ingestion.py
@@ -11,6 +11,9 @@ df = pd.DataFrame(employee_data)
 laxman = {"name": "Laxman", "salary": 1000, "department": "HR"}
 df = pd.concat([df, pd.DataFrame([laxman])], ignore_index=True)
 
+# add role column to the dataframe
+df["role"] = ["Data Scientist", "Consultant", "Operations Manager", "Lead HR"]
+
 # save data to csv file in employee_data directory
 output_dir = Path("employee_data")
 output_dir.mkdir(parents=True, exist_ok=True)  # create directory if it doesn't exist
