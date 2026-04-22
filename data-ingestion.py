47b035e766e6c10ff84855274e3ca8e249a134bc
datta-abhi <rookbee91@gmail.com>
2026-04-22 23:45:48 +0530

    bonus column added

--- data-ingestion.py
+++ data-ingestion.py
@@ -14,6 +14,9 @@ df = pd.concat([df, pd.DataFrame([laxman])], ignore_index=True)
 # add role column to the dataframe
 df["role"] = ["Data Scientist", "Consultant", "Operations Manager", "Lead HR"]
 
+# add bonus logic as 10% of salary for all employees
+df["bonus"] = df["salary"] * 0.10
+
 # save data to csv file in employee_data directory
 output_dir = Path("employee_data")
 output_dir.mkdir(parents=True, exist_ok=True)  # create directory if it doesn't exist
